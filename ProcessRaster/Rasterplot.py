d15a7a3517dab7da137f5cdfc61d19ecf7654e9e
Terryzzyyhh <47970180+Terryzzyyhh@users.noreply.github.com>
2020-10-09 15:26:23 +0800

    默认的

--- ProcessRaster/Rasterplot.py
+++ ProcessRaster/Rasterplot.py
@@ -7,4 +7,4 @@ src = rasterio.open(r"E:\xinjiang\kelamayi_s2\proccessed_files\T45TUK_20200710T0
 pyplot.show()'''
 
 
-show(src.read([1,2,3]),transform=src.transform)
+show(src.read([1,2,3]),transform=src.transform,adjust='linear')
